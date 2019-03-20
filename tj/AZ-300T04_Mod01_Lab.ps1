00b4b57a8e52017e1b63df38501c53e8368f0e53
Thomas Jaekel <tjaekel0815@gmail.com>
2019-03-20 17:24:03 +0100

    new ids

--- tj/AZ-300T04_Mod01_Lab.ps1
+++ tj/AZ-300T04_Mod01_Lab.ps1
@@ -8,10 +8,13 @@ New-AzADServicePrincipal -ApplicationId $aadApp30007.ApplicationId.Guid
 
 # In the output of the New-AzureRmADServicePrincipal command, note the value of the ApplicationId property.
 # You will need this in the next exercise of this lab.
-$ApplicationId = '5dc0a9bc-0c44-4428-bbb3-fcfc34926659'
+$ApplicationId = '6fe11541-1c42-4106-834f-676a7d816185'
 
 $SubscriptionId = Get-AzSubscription | % Id
+$SubscriptionId = '8e98030c-b252-4fbf-98ae-07bde0229d90'
+
 $TenantId = Get-AzSubscription | % TenantId
+$TenantId = 'd9365f9d-75c4-4fa4-8b80-566635202213'
 
 Register-AzResourceProvider -ProviderNamespace Microsoft.EventGrid
 Get-AzResourceProvider
