76c979068aa1ba6141e3d0f7e07d3e57056f595e
Thomas Jaekel <tjaekel0815@gmail.com>
2019-03-22 15:57:40 +0100

    Friday afternoon

--- tj/AZ-300T02_Mod01_Lab.ps1
+++ tj/AZ-300T02_Mod01_Lab.ps1
@@ -4,12 +4,11 @@ Get-AzSubscription
 # Exercise 1:  Implement prerequisites for migration of Azure VMs by using Azure Site Recovery
 # --------------------------------------------------------------------------------------------
 
-$Location1 = "eastus"
-$RGName1 = "az3000601-LabRG"
-New-AzResourceGroup -Name $RGName1 -Location $Location1
+$Location = "westeurope"
+$RGName = "az3000601-LabRG"
+New-AzResourceGroup -Name $RGName -Location $Location
 
 Get-AZResourceGroup | ft ResourceGroupName,Location
-Remove-AzResourceGroup $RGName
 
 # upload the Azure Resource Manager template \allfiles\AZ-300T02\Module_01\azuredeploy06.json into the home directory
 # upload the parameter file \allfiles\AZ-300T02\Module_01\azuredeploy06.parameters.json into the home directory
